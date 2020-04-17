90f0b4f68f85a062afdedf4b581bd626831927d1
Jorge Pezzuol <jorge_pezzuol@hotmail.com>
2020-04-17 15:23:10 +0100

    improving logo validation

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,31 +2,26 @@ document.addEventListener("DOMContentLoaded", () => {
 	//let apiURL = "http://127.0.0.1:3005/api/fetchFood";
 	let apiURL = "https://oquejantar-api.herokuapp.com/api/fetchFood";
 
-	function getCategory(category) {
-
+	function getCategory() {
 		const queryString = window.location.search;
 		const urlParams = new URLSearchParams(queryString);
-		return urlParams.get('categories');
+		return urlParams.get("categories");
 	}
 
 	async function getGeolocate() {
-
 		try {
-
-			let response = await axios.get('https://location.services.mozilla.com/v1/geolocate?key=test');
+			let response = await axios.get(
+				"https://location.services.mozilla.com/v1/geolocate?key=test"
+			);
 			return response.data;
-
-		} catch(err) {
-		}
+		} catch (err) {}
 	}
 
 	async function fetchMenu() {
 		try {
-
 			let geolocate = await getGeolocate();
 
 			let response = await axios.get(apiURL, {
-
 				params: {
 					category: getCategory(),
 					latitude: geolocate.location.lat,
@@ -35,38 +30,52 @@ document.addEventListener("DOMContentLoaded", () => {
 			});
 
 			if (response.status != 200) {
-
 			}
 
-			return response.data.sort( () => Math.random() - 0.5);
+			return response.data.sort(() => Math.random() - 0.5);
 		} catch (err) {}
 	}
 
 	(async () =>
 		fetchMenu().then(data => {
-
-			if(data.length > 0) {
-
+			if (data.length > 0) {
 				for (let food of data) {
-					var logo  = "";
-					
-					if(food.hasOwnProperty('logoUrl') && food.logoUrl != "") {
+					var logo = "";
+
+					if (food.hasOwnProperty("logoUrl") && food.logoUrl != "") {
 						logo = `https://static-images.ifood.com.br/image/upload/f_auto,t_high/pratos/${food.logoUrl}`;
-					}
-					else {
-						logo = `https://static-images.ifood.com.br/image/upload/f_auto,t_thumbnail/logosgde/${food.restaurant.logo.fileName}`;
+					} else {
+						if(food.restaurant.hasOwnProperty("fileName") && food.restaurant.fileName != "") {
+							logo = `https://static-images.ifood.com.br/image/upload/f_auto,t_thumbnail/logosgde/${food.restaurant.logo.fileName}`;
+						}
+						
 					}
 
 					let page = `https://www.ifood.com.br/delivery/${food.restaurant.slug}/${food.restaurant.id}?prato=${food.code}`;
 
-					food.unitPrice = food.unitPrice == 0.0 ? "Escolha uma opção" : `R$ ${food.unitPrice.toFixed(2).toString().replace(".", ",")}`;
+					food.unitPrice =
+						food.unitPrice == 0.0
+							? "Escolha uma opção"
+							: `R$ ${food.unitPrice
+									.toFixed(2)
+									.toString()
+									.replace(".", ",")}`;
 
 					var card = `<div class="tinder--card" data-page=${page}>
 									<h1 class='card-header'>${food.restaurant.name}</h1>
 									<br/>
 									<img crossorigin='anonymous' style='max-height:200px;' src=${logo} />
-									<h3 style='font-style: italic'>${food.hasOwnProperty('description') == true ? food.description : ""}</h3>
-									<p>${food.hasOwnProperty('details') == true ? displayDetails(food.details, 100) : ""}</p>
+									<h3 style='font-style: italic'>${
+										food.hasOwnProperty("description") ==
+										true
+											? food.description
+											: ""
+									}</h3>
+									<p>${
+										food.hasOwnProperty("details") == true
+											? displayDetails(food.details, 100)
+											: ""
+									}</p>
 									<h4>${food.unitPrice}</h4>
 								</div>`;
 
@@ -74,38 +83,28 @@ document.addEventListener("DOMContentLoaded", () => {
 						.querySelector("#tinder-cards")
 						.insertAdjacentHTML("beforeend", card);
 				}
-			}
-
-			else {
-
+			} else {
 				showNoResults();
 			}
-
-
 		}))().then(() => {
 		swipe();
 		document.querySelector("#spinner").remove();
 	});
 
 	function displayDetails(details, maxLength) {
-
-		if(details.length > maxLength) {
+		if (details.length > maxLength) {
 			return `${details.substr(0, maxLength)}...`;
-		}
-		else {
+		} else {
 			return details;
 		}
 	}
 
 	function showNoResults() {
-
-		var card = "<div class='tinder--card'><h1 class='card-header'>Sem resultados</h1></div>";
+		var card =
+			"<div class='tinder--card'><h1 class='card-header'>Sem resultados</h1></div>";
 
 		document
-		.querySelector("#tinder-cards")
-		.insertAdjacentHTML("beforeend", card);
+			.querySelector("#tinder-cards")
+			.insertAdjacentHTML("beforeend", card);
 	}
 });
-
-
-
