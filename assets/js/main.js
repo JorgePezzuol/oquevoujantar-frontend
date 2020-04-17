f7304cd6027eb27252d900726375d4f16a279651
Jorge Pezzuol <jorge_pezzuol@hotmail.com>
2020-04-17 11:03:01 +0100

    fixes to image display and getting the coordinates from the user

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,19 +5,37 @@ document.addEventListener("DOMContentLoaded", () => {
 
 		const queryString = window.location.search;
 		const urlParams = new URLSearchParams(queryString);
-		return urlParams.get('category');
+		return urlParams.get('categories');
+	}
+
+	async function getGeolocate() {
+
+		try {
+
+			let response = await axios.get('https://location.services.mozilla.com/v1/geolocate?key=test');
+			return response.data;
+
+		} catch(err) {
+			console.log(err);
+		}
 	}
 
 	async function fetchMenu() {
 		try {
+
+			let geolocate = await getGeolocate();
+
 			let response = await axios.get(apiURL, {
 
 				params: {
-					category: getCategory()
+					category: getCategory(),
+					latitude: geolocate.location.lat,
+					longitude: geolocate.location.lng
 				}
 			});
 
 			if (response.status != 200) {
+
 			}
 
 			return response.data.sort( () => Math.random() - 0.5);
@@ -26,39 +44,51 @@ document.addEventListener("DOMContentLoaded", () => {
 
 	(async () =>
 		fetchMenu().then(data => {
-			for (let food of data) {
 
-				var logo  = "";
-				
-				if(food.logoUrl != undefined) {
-					logo = `https://static-images.ifood.com.br/image/upload/f_auto,t_high/pratos/${food.logoUrl}`;
+			if(data.length > 0) {
+
+				for (let food of data) {
+					var logo  = "";
+					
+					if(food.hasOwnProperty('logoUrl') && food.logoUrl != "") {
+						logo = `https://static-images.ifood.com.br/image/upload/f_auto,t_high/pratos/${food.logoUrl}`;
+					}
+					else {
+						logo = `https://static-images.ifood.com.br/image/upload/f_auto,t_thumbnail/logosgde/${food.restaurant.logo.fileName}`;
+					}
+
+					let page = `https://www.ifood.com.br/delivery/${food.restaurant.slug}/${food.restaurant.id}?prato=${food.code}`;
+
+					food.unitPrice = food.unitPrice == 0.0 ? "Escolha uma opção" : `R$ ${food.unitPrice.toFixed(2).toString().replace(".", ",")}`;
+
+					var card = `<div class="tinder--card" data-page=${page}>
+									<h1 class='card-header'>${food.restaurant.name}</h1>
+									<br/>
+									<img crossorigin='anonymous' style='max-height:200px;' src=${logo} />
+									<h3 style='font-style: italic'>${food.hasOwnProperty('description') == true ? food.description : ""}</h3>
+									<p>${food.hasOwnProperty('details') == true ? displayDetails(food.details, 100) : ""}</p>
+									<h4>${food.unitPrice}</h4>
+								</div>`;
+
+					document
+						.querySelector("#tinder-cards")
+						.insertAdjacentHTML("beforeend", card);
 				}
-				else {
-					logo = `https://static-images.ifood.com.br/image/upload/f_auto,t_thumbnail/logosgde/${food.restaurant.logo.fileName}`;
-				}
-
-				let page = `https://www.ifood.com.br/delivery/${food.restaurant.slug}/${food.restaurant.id}?prato=${food.code}`;
-
-				food.unitPrice = food.unitPrice == 0.0 ? "" : `R$ ${food.unitPrice.toFixed(2).toString().replace(".", ",")}`;
-
-				var card = `<div class="tinder--card" data-page=${page}>
-								<h1 class='card-header'>${food.restaurant.name}</h1>
-								<img crossorigin='anonymous' style='max-height:200px;' src=${logo} />
-								<h3 style='font-style: italic'>${food.description}</h3>
-								<p>${displayDetais(food.details, 100)}</p>
-								<h4>${food.unitPrice}</h4>
-							</div>`;
-
-				document
-					.querySelector("#tinder-cards")
-					.insertAdjacentHTML("beforeend", card);
 			}
+
+			else {
+
+				showNoResults();
+			}
+
+
 		}))().then(() => {
 		swipe();
+		document.querySelector("#spinner").remove();
 	});
 
-	function displayDetais(details, maxLength) {
-		
+	function displayDetails(details, maxLength) {
+
 		if(details.length > maxLength) {
 			return `${details.substr(0, maxLength)}...`;
 		}
@@ -66,4 +96,16 @@ document.addEventListener("DOMContentLoaded", () => {
 			return details;
 		}
 	}
+
+	function showNoResults() {
+
+		var card = "<div class='tinder--card'><h1 class='card-header'>Sem resultados</h1></div>";
+
+		document
+		.querySelector("#tinder-cards")
+		.insertAdjacentHTML("beforeend", card);
+	}
 });
+
+
+
